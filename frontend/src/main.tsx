91df85efba417c2538fb1065b31b526dca2627d1
Craig MacIntyre <craig.macintyre@thepetlabco.com>
2025-08-18 21:11:26 +0100

    chore: dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
@@ -13,7 +13,6 @@ const queryClient = new QueryClient({
     queries: {
       retry: 1,
       staleTime: 5 * 60 * 1000, // 5 minutes
-      cacheTime: 10 * 60 * 1000, // 10 minutes
     },
   },
 });
